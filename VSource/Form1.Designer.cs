3d11d0178c4d6a6b525f5fe3a1208ce6a3445285
Alan Pies <alan.lee.pies@gmail.com>
2011-11-29 16:59:46 -0800

    first commit

---
+++ VSource/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace VSource
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "EMPLOYEE ID";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(109, 35);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(92, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Last, First, MI.";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 35);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(50, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "SS #";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(35, 220);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "< 300";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ">=300";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(109, 12);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(68, 17);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "First Last";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Gp VLookup Source Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ VSource/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.Dynamics.GP.eConnect;
+using Microsoft.Dynamics.GP.eConnect.Serialization;
+using System.Xml.Linq;
+using System.Collections;
+
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using System.Reflection;
+
+namespace VSource
+{
+    public partial class Form1 : Form
+    {
+
+        public bool employeeIDRequest { get; set; }
+        public bool lastFirstMIRequest { get; set; }
+        public bool socialSecurityRequest { get; set; }
+        public string internalCode { get; set; }
+        public string externallCode { get; set; }
+        public bool firstLastNameRequest { get; set; }
+        
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                employeeIDRequest = true;
+            }
+            else
+            {
+                employeeIDRequest = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            internalCode = textBox1.Text;
+            externallCode = textBox2.Text;
+            
+            // MessageBox.Show(socialSecurityRequest.ToString());
+            try
+            {
+                string pasheet;
+
+                var xpasheet = new XElement("eConnect",
+                    new XElement("RQeConnectOutType",
+                        new XElement("eConnectProcessInfo",
+                          new XElement("Outgoing", "TRUE"),
+                          new XElement("MessageID", "Employee")),
+                        new XElement("eConnectOut",
+                          new XElement("DOCTYPE", "Employee"),
+                          new XElement("OUTPUTTYPE", "2"),
+                          new XElement("FORLOAD", "0"),
+                          new XElement("FORLIST", "1"),
+                          new XElement("ACTION", "0"),
+                          new XElement("ROWCOUNT", "4000"),
+                          new XElement("REMOVE", "0"))));
+
+                pasheet = xpasheet.ToString();
+
+                // Create a connection string to specify the Microsoft Dynamics GP server and database
+                // Change the data source and initial catalog to specify your server and database
+                string sConnectionString = @"data source=VM-GPD01;initial catalog=TMI;integrated security=SSPI;persist security info=False;packet size=4096";
+
+                // Create an eConnectMethods object
+                eConnectMethods requester = new eConnectMethods();
+
+                // Call the eConnect_Requester method of the eConnectMethods object to retrieve specified XML data
+                string reqDoc = requester.eConnect_Requester(sConnectionString, EnumTypes.ConnectionStringType.SqlClient, pasheet);
+
+                // Display the result of the eConnect_Requester method call
+                //Console.Write(reqDoc);
+
+
+                XElement blob = XElement.Parse(reqDoc);
+
+
+
+                //Make Dictionary List of All Departments;
+
+                var deptDictionary = new Dictionary<string, string>()
+                {
+                  {"ACCT","25-" + internalCode +"-10-110-01"},
+                {"ADMIN","25-" + internalCode +"-10-140-01"},
+                {"AMBO","25-" + internalCode +"-40-210-06"},
+                {"AMID","25-" + internalCode +"-30-210-01"},
+                {"AMSC","25-" + internalCode +"-20-210-01"},
+                {"ARCMGT","25-" + externallCode +"-80-900-01"},
+                {"CNTADM","25-" + internalCode +"-10-135-01"},
+                {"CNTAVD","25-" + externallCode +"-20-800-01"},
+                {"CNTRBO","25-" + externallCode +"-40-500-06"},
+                {"CNTRCO","25-" + externallCode +"-20-500-08"},
+                {"CNTRCT","25-" + externallCode +"-20-500-01"},
+                {"CNTRID","25-" + externallCode +"-20-500-06"},
+                {"CNTRMN","25-" + externallCode +"-20-500-07"},
+                {"CNTRNC","25-" + externallCode +"-40-500-03"},
+                {"CNTRSF","25-" + externallCode +"-20-500-05"},
+                {"CNTRTX","25-" + externallCode +"-20-500-03"},
+                {"DMBO","25-" + internalCode +"-20-215-06"},
+                {"EXEC","25-" + internalCode +"-10-100-01"},
+                {"EXECEN","25-" + internalCode +"-10-100-04"},
+                {"FNANCE","25-" + internalCode +"-10-130-01"},
+                {"HR","25-" + internalCode +"-10-120-01"},
+                {"HR ID","25-" + internalCode +"-12-120-06"},
+                {"INFRBO","25-" + externallCode +"-30-700-06"},
+                {"INPRBO","25-" + internalCode +"-30-290-06"},
+                {"INSRBO","25-" + externallCode +"-30-300-06"},
+                {"INSRSC","25-" + externallCode +"-30-300-01"},
+                {"MGDEXC","25-" + internalCode +"-30-280-01"},
+                {"MIS","25-" + internalCode +"-10-160-01"},
+                {"MKTSAL","25-" + internalCode +"-20-205-01"},
+                {"MRKT","25-" + internalCode +"-10-170-01"},
+                {"OCIOSC","25-" + externallCode +"-60-800-01"},
+                {"OPEXEC","25-" + internalCode +"-10-139-01"},
+                {"PRJMGT","25-" + internalCode +"-20-229-01"},
+                {"RCRTBO","25-" + internalCode +"-10-220-06"},
+                {"RCRTEN","25-" + internalCode +"-10-220-04"},
+                {"RCRTM","25-" + internalCode +"-20-220-01"},
+                {"RCRTME","25-" + internalCode +"-10-228-01"},
+                {"RCRTU","25-" + internalCode +"-10-222-01"},
+                {"RCRUIT","25-" + internalCode +"-10-220-01"},
+                {"RMBO","25-" + internalCode +"-10-222-06"},
+                {"SALEBO","25-" + internalCode +"-40-200-06"},
+                {"SALEMG","25-" + internalCode +"-20-209-01"},
+                {"SALES","25-" + internalCode +"-20-200-01"},
+                {"SMSC","25-" + internalCode +"-20-235-01"},
+                {"TRANBO","25-" + externallCode +"-30-310-06"},
+                {"TSG","25-" + internalCode +"-20-230-01"},
+                {"SALEEN","25-" + internalCode +"-20-234-01"},
+                {"TSGMGS","25-" + externallCode +"-60-600-01"},
+                {"CNTRNY","25-" + externallCode +"-20-500-02"},
+                {"","MISSING"}
+
+                };
+
+
+
+
+                //cool stuff down here
+
+
+
+
+
+                int queryCount = blob.Elements("eConnect").Elements("Employee").Count();
+
+
+                string[,] employeeList = new string[queryCount, 10];
+
+                int rowCount = 0;
+                int columnCount = 0;
+                //Console.WriteLine(blob.ToString());
+
+                foreach (XElement employee in blob.Elements("eConnect").Elements("Employee"))
+                {
+                    if (employee.Element("INACTIVE").Value == "0")
+                    {
+                        
+
+                       string mName;
+
+                       if (employeeIDRequest == true)
+                       {
+                           
+                           employeeList[rowCount, columnCount] = employee.Element("FRSTNAME").Value + " " + employee.Element("LASTNAME").Value;
+                           columnCount += 1;
+                       }
+                       if (employeeIDRequest == true)
+                       {
+                          
+                           employeeList[rowCount, columnCount] = employee.Element("FRSTNAME").Value + " " + employee.Element("LASTNAME").Value;
+                           
+                           columnCount += 1;
+                       }
+                       
+                        
+                        
+                       if (lastFirstMIRequest == true)
+                       {
+                           if (employee.Element("MIDLNAME").Value == "")
+                           { mName = ""; }
+                           else
+                           { mName = employee.Element("MIDLNAME").Value.Substring(0, 1) + "."; }
+
+                           employeeList[rowCount, columnCount] = employee.Element("LASTNAME").Value + ", " +
+                               employee.Element("FRSTNAME").Value + " " +
+                                mName;
+
+                           columnCount += 1;
+                       }
+
+                        employeeList[rowCount, columnCount] = employee.Element("DEPRTMNT").Value;
+                        columnCount += 1;
+
+                        if (socialSecurityRequest == true)
+                        {
+                            Func<string, string> socialize = s =>
+                                {
+                                    if (employee.Element("SOCSCNUM").Value == null)
+                                    {
+                                        s = "";
+                                        return s;
+                                    }
+                                    else
+                                    {
+                                        s = employee.Element("SOCSCNUM").Value;
+                                        s = s[0] + s[1] + s[2] + "-" + s[3] + s[4] + "-" + s[5] + s[6] + s[7] + s[8];
+                                        return s;
+                                    }
+
+                                };
+                            employeeList[rowCount, columnCount] = socialize(employee.Element("SOCSCNUM").Value);
+                            columnCount += 1;
+                        } 
+                        
+                        
+                        
+                        
+                        try
+                        {
+                            employeeList[rowCount, columnCount] = deptDictionary[employee.Element("DEPRTMNT").Value];
+                            columnCount += 1;
+                        }
+                        catch
+                        {
+                            employeeList[rowCount, columnCount] = "MISSING";
+                            columnCount += 1;
+                        }
+
+                       
+                        rowCount += 1;
+                        columnCount = 0;
+                    }
+                }
+               
+
+
+
+                //doing excel stuff here
+                Microsoft.Office.Interop.Excel.Application oXL;
+                Microsoft.Office.Interop.Excel._Workbook oWB;
+                Microsoft.Office.Interop.Excel._Worksheet oSheet;
+                Microsoft.Office.Interop.Excel.Range oRng;
+
+                oXL = new Microsoft.Office.Interop.Excel.Application();
+                oXL.Visible = true;
+
+                oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+
+                oSheet.Cells[1, 1] = "EMPLOYEE ID";
+                oSheet.Cells[1, 2] = "THI FORMAT NAME";
+                oSheet.Cells[1, 3] = "Department";
+                oSheet.Cells[1, 4] = "Code";
+
+                string endCornerSheet = "G" + (queryCount + 1).ToString();
+                Excel.Range employeeRange = oSheet.Range["A2", endCornerSheet];
+
+                employeeRange.Value2 = employeeList;
+
+                //oRng = oSheet.get_Range("A2", endCornerSheet).Value2 = employeeRange;
+                oRng = oSheet.get_Range("A2", endCornerSheet).Value2 = employeeList;
+
+               
+
+
+            }
+
+
+            catch (Exception ex)
+            {// Dislay any errors that occur to the console
+               // MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                lastFirstMIRequest = true;
+            }
+            else
+            {
+                lastFirstMIRequest = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                socialSecurityRequest = true;
+            }
+            else
+            {
+                socialSecurityRequest = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                firstLastNameRequest = true;
+            }
+            else
+            {
+                firstLastNameRequest = false;
+            }
+        }
+    }
+}
